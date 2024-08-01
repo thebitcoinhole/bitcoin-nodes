d0cf78fd086d9812ce061a64e9481b7dbe40394c
Maxi Rosson <maxirosson@gmail.com>
2024-08-01 19:20:16 -0300

    Changelog url fix

--- scripts/changelog-url.js
+++ scripts/changelog-url.js
@@ -4,4 +4,7 @@ const itemId = process.argv[2];
 var jsonFile = fs.readFileSync(`../items/${itemId}.json`, 'utf8')
 const jsonData = JSON.parse(jsonFile);
 
-console.log(jsonData[`firmware`][`release-notes`]["links"][0]["url"]);
+if (jsonData[`firmware`][`release-notes`]["links"] && 
+    jsonData[`firmware`][`release-notes`]["links"].length > 0) {
+    console.log(jsonData[`firmware`][`release-notes`]["links"][0]["url"]);
+}
